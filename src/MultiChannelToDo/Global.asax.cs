d22eb525446ab36c5a469de3882932cf3b6bc331
t-inokuchi <t-inokuchi@to-be.co.jp>
2017-08-30 16:00:53 +0900

    add environment property to AI events for server app

--- src/MultiChannelToDo/Global.asax.cs
+++ src/MultiChannelToDo/Global.asax.cs
@@ -2,12 +2,20 @@
 using System.Web.Http;
 using System.Web.Http.Cors;
 
+
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+
 namespace MultiChannelToDo
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            TelemetryConfiguration.Active.TelemetryInitializers.Add(new ConfigInitializer());
+
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
 
             var config = GlobalConfiguration.Configuration.Formatters;
@@ -15,4 +23,19 @@ namespace MultiChannelToDo
             config.Add(new JsonMediaTypeFormatter());
         }
     }
+
+
+    // Begin new code
+    public class ConfigInitializer
+    : ITelemetryInitializer
+    {
+        void ITelemetryInitializer.Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Properties["Environment"] = System.Configuration.ConfigurationManager.AppSettings["environment"];
+        }
+    }
+    // End new code
+
+
+
 }
